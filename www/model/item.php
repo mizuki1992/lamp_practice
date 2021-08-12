17a52577c7ab15b68f439bcf022188533983f428
Mizuki Kuwahara <mzk2468@gmail.com>
2021-08-12 23:03:30 +0900

    item.phpにコメントを追加

--- www/model/item.php
+++ www/model/item.php
@@ -93,6 +93,7 @@ function get_items($db, $is_open = false, $sort = '', $page = NULL){
 }
 
 // 現在のページ数取得用関数
+// 引数：ページ数
 function get_current_page($page){
   // $_GET['page']から値を取得して整数型に変換
   $current_page = (int)(get_get('page'));
@@ -104,6 +105,7 @@ function get_current_page($page){
 }
 
 // 商品数取得用関数
+// 引数：PDO利用
 function count_items($db){
   // count(*)で商品数を取得して、items_amountとして別名をつける
   $sql = '
@@ -129,7 +131,7 @@ function get_all_items($db){
 }
 
 // ステータスが公開になっている商品データ取得用関数
-// 引数：PDO利用
+// 引数１：PDO利用、引数２：並べ替え値、引数３：ページ数
 // 戻り値：商品データ取得用関数（公開ステータスで切り分け）を利用してステータスが公開のデータ
 function get_open_items($db, $sort, $page){
   // 商品データ取得用関数（公開ステータスで切り分け）を利用
@@ -137,29 +139,46 @@ function get_open_items($db, $sort, $page){
   return get_items($db, true, $sort, $page);
 }
 
-
+// 商品登録用関数
+// 引数１：PDO利用、引数２：商品名、引数３：商品価格、引数４：在庫数、引数５：公開ステータス、引数６：商品画像
 function regist_item($db, $name, $price, $stock, $status, $image){
+  // アップロードされたファイル名取得用関数を利用してファイル名を取得
   $filename = get_upload_filename($image);
+  // 入力された商品情報チェック用関数を利用して不正な値が入っていないか確認
   if(validate_item($name, $price, $stock, $filename, $status) === false){
+    // 入力された商品情報のいずれかに不正な値が入っていた場合はfalseを返す
     return false;
   }
+  // 商品情報が正しく入力されていた場合は商品登録処理用関数を利用して商品情報を追加する
   return regist_item_transaction($db, $name, $price, $stock, $status, $image, $filename);
 }
 
+// 商品登録処理用関数
+// 引数１：PDO利用、引数２：商品名、引数３：商品価格、引数４：在庫数、引数５：公開ステータス、引数６：商品画像、引数７：変換された画像ファイル名
 function regist_item_transaction($db, $name, $price, $stock, $status, $image, $filename){
+  // トランザクション開始
   $db->beginTransaction();
+  // 商品情報追加用関数を利用してitemsテーブルに商品情報を追加、アップロードされた画像ファイル保存用関数を利用して画像ファイルを保存
   if(insert_item($db, $name, $price, $stock, $filename, $status) 
     && save_image($image, $filename)){
+    // 商品情報追加、画像ファイルの保存が正しく実行される場合は処理確定（コミット）
     $db->commit();
+    // 商品情報追加、画像ファイルの保存が正しく実行された場合はtrueを返す
     return true;
   }
+  // 商品情報追加、画像ファイルの保存が正しく実行されなかった場合は処理取り消し（ロールバック）
   $db->rollback();
+  // 商品情報追加、画像ファイルの保存が正しく実行されなかった場合はfalseを返す
   return false;
   
 }
 
+// 商品情報追加用関数
+// 引数１：PDO利用、引数２：商品名、引数３：商品価格、引数４：在庫数、引数５：変換された画像ファイル名、引数６：公開ステータス
 function insert_item($db, $name, $price, $stock, $filename, $status){
+  // 公開ステータスの値をopen、closeから1、0に変換
   $status_value = PERMITTED_ITEM_STATUSES[$status];
+  // itemテーブルに商品名、商品価格、在庫数、アップロードされた画像ファイル名、公開ステータスを追加するSQL文
   $sql = "
     INSERT INTO
       items(
@@ -171,11 +190,14 @@ function insert_item($db, $name, $price, $stock, $filename, $status){
       )
     VALUES(:name, :price, :stock, :filename, :status_value);
   ";
-
+  // クエリ実行用関数を利用してSQL文を実行してtrueを返す
   return execute_query($db, $sql, array(':name' => $name, ':price' => $price, ':stock' => $stock, ':filename' => $filename, ':status_value' => $status_value));
 }
 
+// 公開ステータス変更用関数
+// 引数１：PDO利用、引数２：商品ID、引数３：変更後の公開ステータス
 function update_item_status($db, $item_id, $status){
+  //  itemsテーブルの公開ステータスを更新するSQL文
   $sql = "
     UPDATE
       items
@@ -185,11 +207,14 @@ function update_item_status($db, $item_id, $status){
       item_id = :item_id
     LIMIT 1
   ";
-  
+  // クエリ実行用関数を利用してSQL文を実行してtrueを返す
   return execute_query($db, $sql, array(':status' => $status, ':item_id' => $item_id));
 }
 
+// 在庫数変更用関数
+// 引数１：PDO利用、引数２：商品ID、引数３：変更後の在庫数
 function update_item_stock($db, $item_id, $stock){
+  //  itemsテーブルの在庫数を更新するSQL文
   $sql = "
     UPDATE
       items
@@ -199,26 +224,39 @@ function update_item_stock($db, $item_id, $stock){
       item_id = :item_id
     LIMIT 1
   ";
-  
+  // クエリ実行用関数を利用してSQL文を実行してtrueを返す
   return execute_query($db, $sql, array(':stock' => $stock, ':item_id' => $item_id));
 }
 
+// 商品情報削除用関数
+// 引数１：PDO利用、引数２：商品ID
 function destroy_item($db, $item_id){
+  // 商品データ取得用関数を利用して削除したい商品情報を取得
   $item = get_item($db, $item_id);
+  // 商品情報が無い場合はfalseを返す
   if($item === false){
     return false;
   }
+  // 商品情報がある場合はトランザクション開始
   $db->beginTransaction();
+  // テーブルから商品情報削除用関数を利用して該当商品IDの商品情報を削除、保存済み画像ファイル削除用関数を利用して保存場所から画像ファイルを削除
   if(delete_item($db, $item['item_id'])
     && delete_image($item['image'])){
+    // 商品情報削除、画像ファイルの削除が正しく実行できる場合は処理確定（コミット）
     $db->commit();
+    // 商品情報削除、画像ファイルの削除が正しく実行できた場合はtrueを返す
     return true;
   }
+  // 商品情報削除、画像ファイルの削除が正しく実行されなかった場合は処理取り消し（ロールバック）
   $db->rollback();
+  // 商品情報削除、画像ファイルの削除が正しく実行されなかった場合はfalseを返す
   return false;
 }
 
+// テーブルから商品情報削除用関数
+// 引数１：PDO利用、引数２：削除したい商品ID
 function delete_item($db, $item_id){
+  //  itemsテーブルの削除したい商品IDの商品情報を削除するSQL文
   $sql = "
     DELETE FROM
       items
@@ -226,24 +264,36 @@ function delete_item($db, $item_id){
       item_id = :item_id
     LIMIT 1
   ";
-  
+  // クエリ実行用関数を利用してSQL文を実行してtrueを返す
   return execute_query($db, $sql, array(':item_id' => $item_id));
 }
 
 
 // 非DB
 
+// 公開ステータス確認用関数
+// 引数：取得してきた商品データ
 function is_open($item){
+  // 取得してきた商品データの公開ステータスが１だったらtrueを返す
   return $item['status'] === 1;
 }
 
+// 入力された商品情報チェック用関数
+// 引数１：入力された商品名、引数２：入力された商品価格、引数３：入力された在庫数、引数４：アップロードされた画像ファイル名、引数５：選択された公開ステータス
 function validate_item($name, $price, $stock, $filename, $status){
+  // 入力された商品名チェック用関数を利用、正しく商品名が入力されていたらtrue、正しくない場合はfalseが返る
   $is_valid_item_name = is_valid_item_name($name);
+  // 入力された商品価格チェック用関数を利用、正しく商品価格が入力されていたらtrue、正しくない場合はfalseが返る
   $is_valid_item_price = is_valid_item_price($price);
+  // 入力された在庫数チェック用関数を利用、正しく在庫数が入力されていたらtrue、正しくない場合はfalseが返る
   $is_valid_item_stock = is_valid_item_stock($stock);
+  // // アップロードされた画像ファイル名チェック用関数を利用、ファイル形式が正しい場合はtrue、正しくない場合はfalseが返る
   $is_valid_item_filename = is_valid_item_filename($filename);
+  // 選択された公開ステータスチェック用関数を利用、正しく公開ステータスが選択されていたらtrue、正しくない場合はfalseが返る
   $is_valid_item_status = is_valid_item_status($status);
 
+  // 入力された商品名、入力された商品価格、入力された在庫数、、選択された公開ステータスが全てtrueであるか確認
+  // 全てtrueである場合はtrueを返す、一つでもfalseがある場合はfalseを返す
   return $is_valid_item_name
     && $is_valid_item_price
     && $is_valid_item_stock
@@ -251,46 +301,79 @@ function validate_item($name, $price, $stock, $filename, $status){
     && $is_valid_item_status;
 }
 
+// 入力された商品名チェック用関数
+// 引数：入力された商品名
 function is_valid_item_name($name){
+  // 判定用にtrueを代入
   $is_valid = true;
+  // 文字数確認用関数を利用、商品名の最小文字数以上、最大文字数以内ではない場合はfalseが返ってくる
   if(is_valid_length($name, ITEM_NAME_LENGTH_MIN, ITEM_NAME_LENGTH_MAX) === false){
+    // 商品名の最小文字数以上、最大文字数以内ではない場合はエラーメッセージ格納用関数を利用してエラーメッセージを格納
     set_error('商品名は'. ITEM_NAME_LENGTH_MIN . '文字以上、' . ITEM_NAME_LENGTH_MAX . '文字以内にしてください。');
+    // 戻り値用のfalseを代入
     $is_valid = false;
   }
+  // チェック判定結果をtrueかfalseで返す
   return $is_valid;
 }
 
+// 入力された商品価格チェック用関数
+// 引数：入力された商品価格
 function is_valid_item_price($price){
+  // 判定用にtrueを代入
   $is_valid = true;
+  // 正整数チェック用関数関数を利用、正整数で入力されていない場合はfalseが返ってくる
   if(is_positive_integer($price) === false){
+    // 正整数で入力されていない場合はエラーメッセージ格納用関数を利用してエラーメッセージを格納
     set_error('価格は0以上の整数で入力してください。');
+    // 戻り値用のfalseを代入
     $is_valid = false;
   }
+  // チェック判定結果をtrueかfalseで返す
   return $is_valid;
 }
 
+// 入力された在庫数チェック用関数
+// 引数：入力された在庫数
 function is_valid_item_stock($stock){
+  // 判定用にtrueを代入
   $is_valid = true;
+  // 正整数チェック用関数関数を利用、正整数で入力されていない場合はfalseが返ってくる
   if(is_positive_integer($stock) === false){
+    // 正整数で入力されていない場合はエラーメッセージ格納用関数を利用してエラーメッセージを格納
     set_error('在庫数は0以上の整数で入力してください。');
+    // 戻り値用のfalseを代入
     $is_valid = false;
   }
+  // チェック判定結果をtrueかfalseで返す
   return $is_valid;
 }
 
+// アップロードされた画像ファイル名チェック用関数
+// 引数：アップロードされた画像ファイル名
 function is_valid_item_filename($filename){
+  // 判定用にtrueを代入
   $is_valid = true;
+  // アップロードされた画像ファイル名にファイル形式が正しくない場合は空文字が返ってくる
   if($filename === ''){
+    // 戻り値用のfalseを代入
     $is_valid = false;
   }
+  // チェック判定結果をtrueかfalseで返す
   return $is_valid;
 }
 
+// 選択された公開ステータスチェック用関数
+//  引数：選択された公開ステータス（openかclose）
 function is_valid_item_status($status){
+  // 判定用にtrueを代入
   $is_valid = true;
+  // 公開ステータスに値が存在するか確認、存在しない場合はfalseが返ってくる
   if(isset(PERMITTED_ITEM_STATUSES[$status]) === false){
+    // 戻り値用のfalseを代入
     $is_valid = false;
   }
+  // チェック判定結果をtrueかfalseで返す
   return $is_valid;
 }
 
